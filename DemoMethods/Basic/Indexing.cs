2504abe69482951278912826d1304eeb8fd3d55d
aviviadi <avivi.adi.hr@gmail.com>
2015-08-09 19:02:32 +0300

    WIP - Indexing problem

--- DemoMethods/Basic/Indexing.cs
+++ DemoMethods/Basic/Indexing.cs
@@ -10,29 +10,11 @@ namespace DemoMethods.Basic
 {
     public partial class BasicController : ApiController
     {
-        public class Index_DelayedOrder : AbstractIndexCreationTask<Order>
-        {
-            public class Result
-            {
-                public TimeSpan Delay;
-            }
-
-            public Index_DelayedOrder()
-            {
-
-                Map = orders => from order in orders
-                                select new
-                                {
-                                    Delay = order.ShippedAt - order.OrderedAt
-                                };
-            }
-        }
-
-
         public class Index_CostlyOrders : AbstractIndexCreationTask<Order>
         {
             public class Result
             {
+                public string OrderId;
                 public TimeSpan Delay;
                 public int Price;
             }
@@ -42,38 +24,31 @@ namespace DemoMethods.Basic
                 Map = orders => from order in orders
                                 select new
                                 {
+                                    OrderId = order.Id,
                                     Delay = order.ShippedAt - order.OrderedAt,
                                     Price = order.Lines.Sum(x => (x.Quantity * x.PricePerUnit) * (1 - x.Discount))
                                 };
             }
         }
 
-        private const int HIGH_PRICE = 150;
-        private const int DELAY_DAYS = 15;
+        private const int HIGH_PRICE = 1;
+        private const int DELAY_DAYS = 1;
 
         [HttpGet]
         public object Indexing()
         {
-            //GetImportantOrdersWithIssues - High Price Orders and Delayed Orders
+            new Index_CostlyOrders().Execute(Store);
 
+            //GetImportantOrdersWithIssues - High Price Orders and Delayed Orders
             using (var session = Store.OpenSession())
             {
-                List<Order> highPriceOrders = session
-                .Query<Order>()
-                .Where(x => x.Lines.Any(l => l.PricePerUnit > HIGH_PRICE))
-                .ToList();
-
-
-                new Index_DelayedOrder().Execute(Store.DatabaseCommands.ForDatabase("Northwind"), Store.Conventions);
-                new Index_CostlyOrders().Execute(Store.DatabaseCommands.ForDatabase("Northwind"), Store.Conventions);
-
-                List<Order> delayingOrders = session
-                    .Query<Index_CostlyOrders.Result, Index_CostlyOrders>()
-                    .Where(x => x.Delay > TimeSpan.FromDays(DELAY_DAYS) && x.Price > 10)
-                    .OfType<Order>()
+                var problemtaticOrders = session
+                    .Query<Index_CostlyOrders.Result, Index_CostlyOrders>()                    
+                    .Where(x => x.Delay > TimeSpan.FromDays(DELAY_DAYS))// && x.Price > HIGH_PRICE)
+                    //.OfType<Order>()
                     .ToList();
 
-                return DemoUtilities.Instance.ObjectToJson(delayingOrders);
+                return DemoUtilities.Instance.ObjectToJson(problemtaticOrders);
             }
         }
     }
