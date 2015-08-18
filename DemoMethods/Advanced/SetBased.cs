7ecfc8f835063177d7a1a1d4be50d9fcd14a2841
aviviadi <avivi.adi.hr@gmail.com>
2015-08-18 10:35:40 +0300

    WIP - upload/download works fine now. SetBased in progress

--- DemoMethods/Advanced/SetBased.cs
+++ DemoMethods/Advanced/SetBased.cs
@@ -1,4 +1,8 @@
-﻿using System.Web.Http;
+﻿using System.Linq;
+using System.Web.Http;
+using DemoMethods.Indexes;
+using Raven.Abstractions.Data;
+using DemoMethods.Entities;
 
 namespace DemoMethods.Advanced
 {
@@ -7,9 +11,30 @@ namespace DemoMethods.Advanced
         [HttpGet]
         public object SetBased()
         {
-            
-                return DemoUtilities.Instance.ObjectToJson(null);
-            
+            Store.ExecuteIndex(new Index_CompaniesAndCountry());
+
+            Store.DatabaseCommands.UpdateByIndex("Index/CompaniesAndCountry",
+                new IndexQuery {Query = "Country:USA"},
+                new[]
+                {
+                    new PatchRequest
+                    {
+                        Type = PatchCommandType.Modify,
+                        Name = "Country",
+                        Value = "United States of America"
+                    }
+                });            
+
+            using (var session = Store.OpenSession())
+            {
+                var results = session
+                    .Advanced
+                    .DocumentQuery<Company, Index_CompaniesAndAddresses>()
+                    .Search("Address", "USA")
+                    .ToList();
+                    
+                return DemoUtilities.Instance.ObjectToJson(results);
+            }
         }
     }
 }
---
+++ DemoMethods/Indexes/CompanyAndCountryIndex.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using DemoMethods.Entities;
+using Raven.Abstractions.Indexing;
+using Raven.Client.Indexes;
+
+namespace DemoMethods.Indexes
+{
+    public class Index_CompaniesAndCountry : AbstractIndexCreationTask<Company>
+    {
+        public class Result
+        {
+            public Company Company { get; set; }
+            public string Country { get; set; }
+        }
+
+
+        public Index_CompaniesAndCountry()
+        {
+            Map = companies => from company in companies
+                               let a = company.Address
+                               select new
+                               {
+                                   Company = company,
+                                   Country = a.Country
+                               };
+
+            Index(x => x.Address, FieldIndexing.Analyzed);
+            Store(x => x.Address, FieldStorage.Yes);
+            TermVector(x => x.Address, FieldTermVector.WithPositionsAndOffsets);
+
+        }
+    }
+}
