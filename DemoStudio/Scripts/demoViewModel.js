ea3f7fdf473a54d18b9f3d94e1b7b9c66ddf0c72
unknown <Adi@AdiPC.hrhinos.local>
2015-08-24 18:34:36 +0300

    WIP - Knockout tables & html are now filled with results.  Need to adjust each type of response to appropriate view

--- DemoStudio/Scripts/demoViewModel.js
+++ DemoStudio/Scripts/demoViewModel.js
@@ -1,12 +1,20 @@
 // /// <reference path="knockout.d.ts" />
+// /// <reference path="knockout.mapping.d.ts" />
 // /// <reference path="require.d.ts" />
 var demoViewModel = (function () {
     function demoViewModel() {
+        this.isHtml = ko.observable(true);
+        this.htmlView = ko.observable("x");
         this.availableDemos = ko.observableArray(["Choose Demo..."]);
         this.values = ko.observable("");
         this.defdemo = ko.observable();
         this.optionsText = ko.observable();
         this.urlstring = ko.observable();
+        this.isSimpleJson = ko.observable(true);
+        /*columns = ko.observableArray([{ header: "A", dataMember: "B" }, { header: "C", dataMember: "D" }]);
+        rows = ko.observableArray([{ header: "A", dataMember: "B" }, { header: "C", dataMember: "D" }]);*/
+        this.columns = ko.observableArray([]);
+        this.rows = ko.observableArray([]);
         var selfAvailableDemos = this.availableDemos;
         $.ajax("/Menu/Index", "GET").done(function (data) {
             var listOfControllers = data["ListOfControllers"];
@@ -20,12 +28,45 @@ var demoViewModel = (function () {
     return demoViewModel;
 })();
 function runDemo() {
+    var _this = this;
     var url = this.defdemo();
     if (this.values() !== "") {
         url += "?" + this.values();
     }
     $.ajax(url, "GET").done(function (data) {
         console.log(data);
+        var jsonObj = data;
+        if (typeof (data) === "string") {
+            _this.htmlView(data);
+            return;
+        }
+        if (data instanceof Array === false) {
+            jsonObj = [data];
+        }
+        //var newArr = [];
+        _this.columns([]);
+        _this.rows([]);
+        for (var i = 0; i < jsonObj.length; i++) {
+            var item = jsonObj[i];
+            var newItem = {};
+            for (var key in item) {
+                if (i === 0)
+                    _this.columns.push(key);
+                if (typeof item[key] !== "object") {
+                    newItem[key] = item[key]; // copy the new item
+                }
+                else {
+                    for (var deeperKey in item[key]) {
+                        _this.columns.push(deeperKey);
+                        // var obj = {};
+                        // obj[deeperKey] = item[key][deeperKey];
+                        // itemsToBeCreated.push(obj);
+                        newItem[deeperKey] = item[key][deeperKey];
+                    }
+                }
+            }
+            _this.rows.push(newItem);
+        }
     });
 }
 function genUrl() {
--- DemoStudio/Scripts/demoViewModel.ts
+++ DemoStudio/Scripts/demoViewModel.ts
@@ -1,4 +1,5 @@
 ﻿// /// <reference path="knockout.d.ts" />
+// /// <reference path="knockout.mapping.d.ts" />
 // /// <reference path="require.d.ts" />
 
 
@@ -6,17 +7,28 @@ declare var ko;
 declare var availableDemos;
 
 class demoViewModel {
+    isHtml = ko.observable(true);
+    htmlView = ko.observable("x");
     availableDemos = ko.observableArray(["Choose Demo..."]);
     values = ko.observable("");
     defdemo = ko.observable();
     optionsText = ko.observable();
     urlstring = ko.observable();
 
+    
+    isSimpleJson = ko.observable(true);
+
+    /*columns = ko.observableArray([{ header: "A", dataMember: "B" }, { header: "C", dataMember: "D" }]);
+    rows = ko.observableArray([{ header: "A", dataMember: "B" }, { header: "C", dataMember: "D" }]);*/
+    columns = ko.observableArray([]);
+    rows = ko.observableArray([]);
+    
+
     constructor() {
         var selfAvailableDemos = this.availableDemos;
         $.ajax("/Menu/Index", "GET").done(data => {
             var listOfControllers = data["ListOfControllers"];
-
+            
             listOfControllers.forEach(function(entry) {
                 selfAvailableDemos.push(entry);
             });
@@ -27,16 +39,75 @@ class demoViewModel {
 }
 
 
+
 function runDemo(): void {
-    var url = this.defdemo();
+    var url = this.defdemo();    
+
     if (this.values() !== "") {
         url += "?" + this.values();
     }
+
+    
     $.ajax(url, "GET").done(data => {
         console.log(data);
+
+        var jsonObj = data;
+
+        if (typeof (data) === "string") {
+            this.htmlView(data);
+            return;
+        }
+
+
+        if (data instanceof Array === false) {
+            jsonObj = [data];
+        }
         
+
+        //var newArr = [];
+        this.columns([]);
+        this.rows([]);
+        for (var i = 0; i < jsonObj.length; i++) {
+            var item = jsonObj[i];
+
+            var newItem = {};
+            // var itemsToBeCreated = []; // holds new items that need to be created
+
+            for (var key in item) {
+                if (i === 0)
+                    this.columns.push(key);
+
+                if (typeof item[key] !== "object") {
+                    newItem[key] = item[key]; // copy the new item
+                } else {
+                    for (var deeperKey in item[key]) {
+                        this.columns.push(deeperKey);
+                        // var obj = {};
+                        // obj[deeperKey] = item[key][deeperKey];
+                        // itemsToBeCreated.push(obj);
+                        newItem[deeperKey] = item[key][deeperKey];
+                    }
+                }
+            }
+
+            this.rows.push(newItem);
+
+            //for (var y = 0; y < itemsToBeCreated.length; y++) {
+            //    this.rows.push(jQuery.extend(itemsToBeCreated[y], newItem));
+            //}
+        }
+
+
+
+
+
     });
+
+
 }
+
+
+
 
 function genUrl(): void {
     
