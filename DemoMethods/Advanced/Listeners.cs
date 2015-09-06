0b4b3bf8e8f640ed4402a3ff3d77dcbe2ca32eff
aviviadi <Adi@AdiPC.hrhinos.local>
2015-09-06 13:18:37 +0300

    WIP - Listener fixed

--- DemoMethods/Advanced/Listeners.cs
+++ DemoMethods/Advanced/Listeners.cs
@@ -1,5 +1,9 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Web;
 using System.Web.Http;
+using System.Web.UI.WebControls;
 using DemoMethods.Indexes;
 using Raven.Client;
 using Raven.Client.Listeners;
@@ -21,17 +25,27 @@ namespace DemoMethods.Advanced
         [HttpGet]
         public object Listeners()
         {
-            //TODO: need to _remove_ this afterward, otherwise posions rests
-             DocumentStoreHolder.Store.Listeners.RegisterListener(new UsaOnlyQueryListener());
+            DocumentStoreHolder.Store.Listeners.RegisterListener(new UsaOnlyQueryListener());
 
-
+            List<IndexNameAndCountry.Result> results;
             using (var session = DocumentStoreHolder.Store.OpenSession())
             {
 
-                var result = session.Query<IndexNameAndCountry.Result, IndexNameAndCountry>();
-                //TODO: add support for querying with something else here
-                return (result.ToList());
+                var query = session.Query<IndexNameAndCountry.Result, IndexNameAndCountry>() as IQueryable<IndexNameAndCountry.Result>;
+
+                var nvc = HttpUtility.ParseQueryString(Request.RequestUri.Query);
+                var name = nvc["name"];
+                if (string.IsNullOrEmpty(name) == false)
+                {
+                    query = query.Where(x => x.Name.StartsWith(name));
+                }
+
+                results = query.ToList();
+
             }
+            DocumentStoreHolder.Store.Listeners.QueryListeners = new IDocumentQueryListener[0];
+
+            return results;
         }
     }
 }
