1abde3a5882202105dc2601a164787f83881b6b9
aviviadi <avivi.adi.hr@gmail.com>
2015-09-10 18:02:11 +0300

    REL 0.1 - and added with dynamic facets

---
+++ DemoMethods/Basic/FacetsDynamicRange.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using DemoMethods.Entities;
+using DemoMethods.Indexes;
+using Raven.Abstractions.Data;
+using Raven.Client;
+
+namespace DemoMethods.Basic
+{
+    public partial class BasicController : ApiController
+    {
+        [HttpGet]
+        public object FacetsDynamicRange(string fromVal = "10", string toVal = "20")
+        {
+            var from = decimal.Parse(fromVal);
+            var to = decimal.Parse(toVal);
+
+            List<Facet> newFacet = new List<Facet>()
+            {
+                new Facet
+                {
+                    Name = "Products"
+                },
+                new Facet<Product>
+                {
+                    Name = x => x.PricePerUnit,
+                    Ranges =
+                    {
+                        x => x.PricePerUnit < from,
+                        x => x.PricePerUnit > from && x.PricePerUnit < to,
+                        x => x.PricePerUnit > to,
+                    }
+                },
+                new Facet<Product>
+                {
+                    Name = x => x.UnitsInStock,
+                    Ranges =
+                    {
+                        x => x.UnitsInStock < 10,
+                        x => x.UnitsInStock > 10
+                    }
+                }
+            };
+
+            using (var session = DocumentStoreHolder.Store.OpenSession())
+            {
+                var facetResults = session
+                    .Query<Product, ProductsAndPriceAndSuplier>()
+                    .Where(x => x.UnitsInStock > 1)
+                    .ToFacets(newFacet);
+
+                return DemoUtilities.FormatRangeResults(facetResults.Results);
+            }
+        }
+    }
+}
---
+++ DemoMethods/Menu/CreateFixedFacet.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using DemoMethods.Indexes;
+using Raven.Abstractions.Data;
+
+namespace DemoMethods
+{
+    public partial class MenuController : ApiController
+    {
+        public static List<Facet> FixedFacet { get; set; }
+        public void CreateFixedFacet()
+        {
+            decimal fromVal = 10;
+            decimal toVal = 20;
+            FixedFacet = FacetRangeCreation.CreateFacets(fromVal, toVal);
+            using (var session = DocumentStoreHolder.Store.OpenSession())
+            {
+                session.Store(new FacetSetup { Id = "facets/ProductFacet", Facets = FixedFacet });
+                session.SaveChanges();
+            }
+        }
+    }
+}
