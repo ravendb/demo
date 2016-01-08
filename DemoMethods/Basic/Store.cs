683d19ba292372c198284e9ee6dd3b0c3dd8d82b
aviviadi <avivi.adi.hr@gmail.com>
2016-01-08 15:43:58 +0200

    Shorten store demo

--- DemoMethods/Basic/Store.cs
+++ DemoMethods/Basic/Store.cs
@@ -10,31 +10,21 @@ namespace DemoMethods.Basic
         [Demo("Store", DemoOutputType.String, demoOrder: 10)]
         public object Store()
         {
-            var address = new Address
-            {
-                Country = "IL",
-                City = "Hadera",
-                Line1 = "Kombe 10"
-            };
-
-            var newCompany = new Company
-            {
-                Name = "Hibernating Rhinos",
-                ExternalId = "HR",
-                Phone = "+972 4 622 7811",
-                Fax = "+972 153 4 622 7811",
-                Address = address
-            };
-
             using (var session = DocumentStoreHolder.Store.OpenSession())
             {
-                session.Store(newCompany);
+                session.Store(new Company
+                {
+                    Name = "Hibernating Rhinos",
+                    ExternalId = "HR",
+                    Phone = "+972 4 622 7811",
+                    Fax = "+972 153 4 622 7811",
+                });
+
                 session.SaveChanges();
 
-                return $"{newCompany.Id} Created Successfully";
+                return $"New Company Created Successfully";
             }
         }
-
     }
 }
 
