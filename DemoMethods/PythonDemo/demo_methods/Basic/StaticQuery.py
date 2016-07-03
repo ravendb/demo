78ed292da0657d1b8e06ec1109107b60fea67aa0
IdanHaim <haimdude@gmail.com>
2016-07-03 11:33:58 +0300

    Remove unnecessarily

--- DemoMethods/PythonDemo/demo_methods/Basic/StaticQuery.py
+++ DemoMethods/PythonDemo/demo_methods/Basic/StaticQuery.py
@@ -8,8 +8,9 @@ class StaticQuery(object):
     def static_query(high_price=500, delay_days=35):
         with DocumentStoreHolder.get_store().open_session() as session:
             query = list(
-                session.query(index_name="CostlyOrders").where_greater_than(
-                    "Price", high_price).and_also().where_greater_than("Delay", Utils.timedelta_tick(
-                        timedelta(days=delay_days))))
+                session.query(index_name="CostlyOrders").
+                where_greater_than("Price", high_price).
+                and_also().
+                where_greater_than("Delay", Utils.timedelta_tick(timedelta(days=delay_days))))
 
             return query[0] if query else None
