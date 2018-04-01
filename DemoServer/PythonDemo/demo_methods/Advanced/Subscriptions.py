c7a5f09dec2503ee62eb5c04021ef6ac4f67594b
Idan Haim Shalom <haimdude@gmail.com>
2018-04-01 19:19:58 +0300

    RDoc-1180 Add Subscriptions for C# and python

---
+++ DemoServer/PythonDemo/demo_methods/Advanced/Subscriptions.py
@@ -0,0 +1,27 @@
+﻿from demo.holder import DocumentStoreHolder
+from pyravendb.subscriptions.data import SubscriptionCreationOptions, SubscriptionWorkerOptions
+from pyravendb.subscriptions.data import SubscriptionOpeningStrategy
+from pyravendb.custom_exceptions.exceptions import SubscriptionClosedException
+
+
+class Subscriptions(object):
+
+    @staticmethod
+    def subscriptions(subscriptionName=None):
+        companies = []
+        with DocumentStoreHolder.get_store() as store:
+            if not subscriptionName:
+                subscription_creation_options = SubscriptionCreationOptions("From Companies")
+                subscriptionName = store.subscriptions.create(subscription_creation_options)
+
+            worker_options = SubscriptionWorkerOptions(subscriptionName, strategy=SubscriptionOpeningStrategy.take_over,
+                                                       close_when_no_docs_left=True)
+            with store.subscriptions.get_subscription_worker(worker_options) as subscription_worker:
+                try:
+                    subscription_worker.run(
+                        partial(lambda batch, x=companies: x.extend([item.raw_result for item in batch.items]))).join()
+                except SubscriptionClosedException:
+                    # That's expected
+                    pass
+
+        return companies
