f0676cb8f398017718dd698dc9a87ee12ce8d930
Rafał Kwiatkowski <kwiato.r@gmail.com>
2018-10-10 11:40:34 +0200

    walkthrough elements

--- DemoServer/client/src/components/ui/demo/body/index.tsx
+++ DemoServer/client/src/components/ui/demo/body/index.tsx
@@ -23,10 +23,15 @@ export class DemoBody extends React.Component<DemoBodyProps, DemoBodyState> {
         const resultsId = "results";
         return <div className="demo-body">
             <div id="demo-body-container">
+            <div className="walkthrough-top"></div>
             <div id="demo-highlight">
                 <div id="highlight-top"></div>
                 <div id="highlight-right"></div>
-                <div id="highlight-bottom"></div>
+                <div id="highlight-bottom">
+                    <div className="walkthrough-description">
+                        <h3>Lorem ipsum dolor sit amet</h3>
+                    </div>
+                </div>
                 <div id="highlight-left"></div>
             </div>
             <Parameters items={[
