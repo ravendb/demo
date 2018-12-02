973a737453eb0cf12c2334fd3792a86816b19168
Danielle Greenberg <danielle.9897@gmail.com>
2018-12-02 12:08:51 +0200

    [client] Next button at end of walkthrough description

--- DemoServer/client/src/components/demoDisplay/walkthrough/WalkthroughDescription.tsx
+++ DemoServer/client/src/components/demoDisplay/walkthrough/WalkthroughDescription.tsx
@@ -1,11 +1,11 @@
 import * as React from "react";
 import { connect } from "react-redux";
 import { AppState } from "../../../store/state";
-import { getCurrentWalkthrough } from "../../../store/state/DemoState";
+import { getCurrentWalkthrough, getCurrentWalkthroughIndex} from "../../../store/state/DemoState";
 import { Markdown } from "../../helpers/Markdown";
 import { DemoLinkDto } from "../../../models/dtos";
-import { IconRight } from "../../helpers/icons";
-import { Header } from "../../ui/Header";
+import { IconRight, IconCancel } from "../../helpers/icons";
+import { getUrlWithoutWalkthrough, geWalkthroughUrls} from "../../../store/helpers/walkthroughUrls";
 
 interface DemoLinkDisplayProps {
     url: string;
@@ -25,17 +25,19 @@ interface Props {
     title: string;
     slug: string;
     demoLink: DemoLinkDto;
+    nextStepUrl: string;
+    numberOfSteps: number;
+    closeUrl: string;
 }
 
 function WalkthroughDescriptionComponent(props: Props) {
-    const { descriptionHtml, title, slug, demoLink } = props;
+    const { descriptionHtml, title, slug, demoLink, nextStepUrl, numberOfSteps, closeUrl } = props;
 
     const stepNumber = slug && slug.split("-")[1];
 
     return <div className="walkthrough-step">
         <header>
             <h2> Step {stepNumber} : {title}</h2>
-            <a href="" className="nextStep"> Next step <IconRight /></a>
         </header>
         <div className="description">
             {
@@ -44,19 +46,36 @@ function WalkthroughDescriptionComponent(props: Props) {
                     : <Markdown>{descriptionHtml}</Markdown>
             }
         </div>
+        <footer>
+            {
+                stepNumber === numberOfSteps.toString()
+                    ? <a href={closeUrl} className="nextStep"> Close <IconCancel /></a>
+                    : <a href={nextStepUrl} className="nextStep"> Next Step <IconRight/></a>
+            }
+        </footer>
     </div>;
 }
 
 export const WalkthroughDescription = connect<Props>(
     ({ demos }: AppState): Props => {
+
         const wt = getCurrentWalkthrough(demos);
+        const currentWtIndex = getCurrentWalkthroughIndex(demos);
+        
+        const wtUrls = geWalkthroughUrls(demos);
+        const nextStepUrl = wtUrls[currentWtIndex+1];
+        const numberOfSteps = wtUrls.length;
+        const closeUrl = getUrlWithoutWalkthrough(demos);
 
         if (!wt) {
             return {
                 descriptionHtml: null,
                 title: null,
                 slug: null,
-                demoLink: null
+                demoLink: null,
+                nextStepUrl: null,
+                numberOfSteps: 0,
+                closeUrl: null
             }
         }
         const { descriptionHtml, title, slug, demoLink } = wt;
@@ -65,7 +84,10 @@ export const WalkthroughDescription = connect<Props>(
             descriptionHtml,
             title,
             slug,
-            demoLink
-        };
+            demoLink,
+            nextStepUrl,
+            numberOfSteps,
+            closeUrl
+        }
     }
 )(WalkthroughDescriptionComponent);
