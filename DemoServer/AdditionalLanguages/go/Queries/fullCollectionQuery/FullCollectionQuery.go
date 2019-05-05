11a97049c3a95e55d48a956c2db5159fdf7b3a9c
Danielle Greenberg <danielle.9897@gmail.com>
2019-05-05 16:37:01 +0300

    [GO Demos] Query-Overview + Full-Collection-Query Demos

---
+++ DemoServer/AdditionalLanguages/go/Queries/fullCollectionQuery/FullCollectionQuery.go
@@ -0,0 +1,71 @@
+package main
+
+//region Usings
+import "github.com/ravendb/ravendb-go-client"
+//endregion
+
+var globalDocumentStore *ravendb.DocumentStore
+
+func main() {
+    createDocumentStore()
+    createDatabase()
+    fullCollectionQuery()
+    globalDocumentStore.Close()
+}
+
+func createDocumentStore() (*ravendb.DocumentStore, error) {
+    if globalDocumentStore != nil {
+        return globalDocumentStore, nil
+    }
+    urls := []string{"http://localhost:8080"}
+    store := ravendb.NewDocumentStore(urls, "testGO")
+    err := store.Initialize()
+    if err != nil {
+        return nil, err
+    }
+    globalDocumentStore = store
+    return globalDocumentStore, nil
+}
+
+func createDatabase() {
+    databaseRecord := ravendb.NewDatabaseRecord()
+    databaseRecord.DatabaseName = "testGO"
+    createDatabaseOperation := ravendb.NewCreateDatabaseOperation(databaseRecord, 1)
+    var err = globalDocumentStore.Maintenance().Server().Send(createDatabaseOperation)
+    if err != nil {
+        fmt.Printf("d.store.Maintenance().Server().Send(createDatabaseOperation) failed with %s\n", err)
+    }
+}
+
+type Company struct {
+    ID      string
+    Name    string
+    Phone   string
+    Contact *Contact
+}
+
+//region Demo
+func fullCollectionQuery() error {
+    
+    session, err := globalDocumentStore.OpenSession("")
+    if err != nil {
+        return err
+    }
+    defer session.Close()
+
+    //region Step_1
+    queriedType := reflect.TypeOf(&Company{})
+    fullCollectionQuery := session.QueryCollectionForType(queriedType)
+    //endregion
+
+    //region Step_2
+    var collectionResults []*Company
+    err = fullCollectionQuery.GetResults(&collectionResults)
+    if err != nil {
+        return err
+    }
+    //endregion
+
+    return nil
+}
+//endregion
