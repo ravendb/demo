52fea2d24e9b924051899daf98001094c5630e08
Marcin Lewandowski <marcin@ravendb.net>
2020-01-08 14:19:12 +0100

    more demos

---
+++ DemoServer/AdditionalLanguages/java/src/main/java/net/ravendb/demo/queries/filteringResultsBasics/FilteringResultsBasics.java
@@ -0,0 +1,29 @@
+package net.ravendb.demo.queries.filteringResultsBasics;
+
+import net.ravendb.client.documents.session.IDocumentQuery;
+import net.ravendb.client.documents.session.IDocumentSession;
+import net.ravendb.demo.common.DocumentStoreHolder;
+import net.ravendb.demo.common.models.Employee;
+
+import java.util.List;
+
+public class FilteringResultsBasics {
+    public void run() {
+        //region Demo
+        List<Employee> filteredEmployees;
+
+        try (IDocumentSession session = DocumentStoreHolder.store.openSession()) {
+            //region Step_1
+            IDocumentQuery<Employee> filteredQuery = session.query(Employee.class)
+            //endregion
+                //region Step_2
+                .whereEquals("firstName", "Anne");
+                //endregion
+
+            //region Step_3
+            filteredEmployees = filteredQuery.toList();
+            //endregion
+        }
+        //endregion
+    }
+}
