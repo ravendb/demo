2918f00fd523954f74c43ee0d187d2ad9725a21e
Marcin Lewandowski <marcin@ravendb.net>
2020-01-08 12:15:19 +0100

    created models for java examples

---
+++ DemoServer/AdditionalLanguages/java/src/main/java/net/ravendb/demo/common/models/Territory.java
@@ -0,0 +1,32 @@
+package net.ravendb.demo.common.models;
+
+public class Territory {
+    private String code;
+    private String name;
+    private String area;
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getArea() {
+        return area;
+    }
+
+    public void setArea(String area) {
+        this.area = area;
+    }
+
+}
