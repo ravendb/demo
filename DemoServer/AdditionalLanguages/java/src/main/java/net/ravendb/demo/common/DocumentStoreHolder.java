267ec1ec9264cda4bd27b415c51b1e56474442d3
Marcin Lewandowski <marcin@ravendb.net>
2020-01-08 12:29:33 +0100

    related documents example

---
+++ DemoServer/AdditionalLanguages/java/src/main/java/net/ravendb/demo/common/DocumentStoreHolder.java
@@ -0,0 +1,8 @@
+package net.ravendb.demo.common;
+
+import net.ravendb.client.documents.DocumentStore;
+
+public class DocumentStoreHolder {
+
+    public static DocumentStore store = new DocumentStore();
+}
