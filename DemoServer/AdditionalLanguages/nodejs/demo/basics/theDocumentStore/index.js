74173e1e4f5cbd36475681b855a6aa1af4c9b59c
Danielle Greenberg <danielle.9897@gmail.com>
2020-01-22 14:28:49 +0200

    [Demo] Adding NodeJs Demo

--- DemoServer/AdditionalLanguages/nodejs/demo/basics/theDocumentStore/index.js
+++ DemoServer/AdditionalLanguages/nodejs/demo/basics/theDocumentStore/index.js
@@ -2,28 +2,19 @@
 const { DocumentStore } = require('ravendb');
 //endregion
 
-// there's less steps here than in java or C# due to singleton nature of node.js module
-// not sure how they should be laid out
-
 //region Demo
 //region Step_1
+const serverUrl = 'http://localhost:8080';
+const databaseName = 'YourDatabaseName';
+
+const store = new DocumentStore(serverUrl, databaseName);
 //endregion
 
 //region Step_2
-const serverUrl = 'http://localhost:8080';
-const databaseName = 'YourDatabaseName';
-//endregion
-
-//region Step_3
-const store = new DocumentStore(serverUrl, databaseName);
-//endregion
-//region Step_4
-//endregion
-//region Step_5
 store.initialize();
 //endregion
 
-//region Step_6
+//region Step_3
 module.exports = { store };
 //endregion
 //endregion
