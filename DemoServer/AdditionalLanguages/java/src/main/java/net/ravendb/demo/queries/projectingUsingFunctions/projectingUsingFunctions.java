8e439305521c1f9b4db3657ccb72efd4f3fea489
Drorlev <dror@ravendb.net>
2022-01-06 16:01:00 +0200

    HRINT-2417_nodejs_java - fixes

--- DemoServer/AdditionalLanguages/java/src/main/java/net/ravendb/demo/queries/projectingUsingFunctions/projectingUsingFunctions.java
+++ DemoServer/AdditionalLanguages/java/src/main/java/net/ravendb/demo/queries/projectingUsingFunctions/projectingUsingFunctions.java
@@ -11,35 +11,35 @@ import java.util.List;
 public class projectingUsingFunctions {
 
     //region Demo
-    public static class Employee {
+    public static class EmployeeDetails {
         public String FullName;
         public String Title;
     }
     //endregion
 
 
-    public List<Employee> run() {
-        List<Employee> projectedResults;
+    public List<EmployeeDetails> run() {
+        List<EmployeeDetails> projectedResults;
         //region Demo
         try (IDocumentSession session = DocumentStoreHolder.store.openSession()) {
         //region Step_1
             String rawQueryString =  "declare function output(employee) {" +
-                    "var formatName  = function(employee) { return 'FullName: ' + employee.FirstName + ' ' + employee.LastName; };" +
-                    "var formatTitle = function(employee) { return 'Title: ' + employee.Title };" +
-                    "return { Title : formatTitle(employee), FullName : formatName(employee) };" +
+                    "   var formatName  = function(employee) { return 'FullName: ' + employee.FirstName + ' ' + employee.LastName; };" +
+                    "   var formatTitle = function(employee) { return 'Title: ' + employee.Title };" +
+                    "   return { Title : formatTitle(employee), FullName : formatName(employee) };" +
                     "}"+
         //endregion
         //region Step_2
-                    "from Employees as employee select output(employee)");
+                    "from Employees as employee select output(employee)";
         //endregion
         
         //region Step_3
-            IRawDocumentQuery<Employee> projectedQueryWithFunctions = session.advanced()
-            .rawQuery(Employee.class,rawQueryString);
+            IRawDocumentQuery<EmployeeDetails> projectedQueryWithFunctions = session.advanced()
+                .rawQuery(EmployeeDetails.class, rawQueryString);
         //endregion
         
         //region Step_4
-             projectedResults = projectedQueryWithFunctions.toList();
+            projectedResults = projectedQueryWithFunctions.toList();
         //endregion
         //endregion
     }
