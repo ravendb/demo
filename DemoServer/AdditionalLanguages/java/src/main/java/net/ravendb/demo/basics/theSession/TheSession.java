9cbc85de196bcd889bb90a453c5943d8d994b479
Marcin Lewandowski <marcin@ravendb.net>
2022-03-04 09:53:03 +0100

    HRINT-2594 Java part of demo doesn't compile

--- DemoServer/AdditionalLanguages/java/src/main/java/net/ravendb/demo/basics/theSession/TheSession.java
+++ DemoServer/AdditionalLanguages/java/src/main/java/net/ravendb/demo/basics/theSession/TheSession.java
@@ -1,28 +1,31 @@
 package net.ravendb.demo.basics.theSession;
 
 //region Usings
-import net.ravendb.client.documents.IDocumentStore;
 import net.ravendb.client.documents.session.IDocumentSession;
 //endregion
 import net.ravendb.demo.common.DocumentStoreHolder;
 
 public class TheSession {
-    //region Demo
-    //region Step_1
-    IDocumentSession session = DocumentStoreHolder.getStore().openSession("YourDatabaseName");
-    //endregion
 
-    //region Step_2
-    //   Run your business logic:
-    //
-    //   Store documents
-    //   Load and Modify documents
-    //   Query indexes & collections
-    //   Delete docum
-    //endregion
+    public void run() throws Exception {
+        //region Demo
+        //region Step_1
+        IDocumentSession session = DocumentStoreHolder.store.openSession("YourDatabaseName");
+        //endregion
 
-    //region Step_3
-    session.saveChanges();
-    //endregion
-    //endregion
+        //region Step_2
+        //   Run your business logic:
+        //
+        //   Store documents
+        //   Load and Modify documents
+        //   Query indexes & collections
+        //   Delete document
+        //   .... etc.
+        //endregion
+
+        //region Step_3
+        session.saveChanges();
+        //endregion
+        //endregion
+    }
 }
