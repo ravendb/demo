f74266723222206e1c61b22bd2bdd9bb6b721579
danielle9897 <danielle.9897@gmail.com>
2022-08-15 12:06:58 +0300

    HRINT-2958 [Demo] Project Index Results

--- DemoServer/AdditionalLanguages/nodejs/demo/staticIndexes/projectIndexResults/index.js
+++ DemoServer/AdditionalLanguages/nodejs/demo/staticIndexes/projectIndexResults/index.js
@@ -1,24 +1,27 @@
 const { documentStore } = require('../../common/docStoreHolder');
-const {
-    AbstractJavaScriptIndexCreationTask,
-    QueryData
-} = require('ravendb');
-const {
-    Employee
-} = require('../../common/models');
+const { Employee } = require('../../common/models');
+//region Usings
+const { AbstractJavaScriptIndexCreationTask, QueryData } = require('ravendb');
+//endregion
 
+//region Demo
+//region Step_1
 class Employees_ByWorkPeriod extends AbstractJavaScriptIndexCreationTask {
+//endregion
     constructor () {
         super();
 
+        //region Step_2
         this.map('employees', employee => {
             return {
                 workingInCompanySince: new Date(employee.HiredAt).getFullYear()
             };
         });
+        //endregion
     }
 }
 
+//region Step_3
 class EmployeeProjectedDetails {
     constructor () {
         this.firstName = null;
@@ -26,25 +29,34 @@ class EmployeeProjectedDetails {
         this.location = null;
     }
 }
+//endregion
+//endregion
 
 async function run ({ startYear }) {
     await new Employees_ByWorkPeriod().execute(documentStore);
 
+    //region Demo
     const session = documentStore.openSession();
 
     const employeesQuery = session
+        //region Step_4
         .query(Employee, Employees_ByWorkPeriod)
         .whereGreaterThan('workingInCompanySince', startYear)
+        //endregion
+        //region Step_5
         .selectFields(QueryData.customFunction('employee',
-            '{ firstName: employee.FirstName,\n' +
-            '  phone: employee.HomePhone,\n' +
+            '{ firstName: employee.FirstName,' +
+            '  phone: employee.HomePhone,' +
             "  location: employee.Address.City + ' ' + employee.Address.Country }"),
         EmployeeProjectedDetails);
+        //endregion
 
+    //region Step_6
     const employeesSinceYear = await employeesQuery.all();
+    //endregion
+    //endregion
 
     return employeesSinceYear;
 }
-
 
 module.exports = { run };
