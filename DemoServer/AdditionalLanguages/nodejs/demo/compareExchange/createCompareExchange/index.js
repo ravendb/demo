37596e5f8a83855081afd9473a1f17a57be24c89
danielle9897 <danielle.9897@gmail.com>
2022-08-16 18:22:21 +0300

    HRINT-2964 [Demo] Create Compare Exchange

--- DemoServer/AdditionalLanguages/nodejs/demo/compareExchange/createCompareExchange/index.js
+++ DemoServer/AdditionalLanguages/nodejs/demo/compareExchange/createCompareExchange/index.js
@@ -1,8 +1,7 @@
 const { documentStore } = require('../../common/docStoreHolder');
-const {
-    PutCompareExchangeValueOperation,
-    GetCompareExchangeValueOperation
-} = require('ravendb');
+//region Usings
+const { PutCompareExchangeValueOperation, GetCompareExchangeValueOperation } = require('ravendb');
+//endregion
 
 const DEFAULT_COMPARE_EXCHANGE_KEY = 'abc@gmail.com';
 const DEFAULT_COMPARE_EXCHANGE_VALUE = 'employee/1-A';
@@ -11,12 +10,16 @@ async function run ({ cmpXchgKey, cmpXchgValue }) {
     cmpXchgKey = cmpXchgKey || DEFAULT_COMPARE_EXCHANGE_KEY;
     cmpXchgValue = cmpXchgValue || DEFAULT_COMPARE_EXCHANGE_VALUE;
 
+    //region Demo
     let result;
 
+    //region Step_1
     const putCmpXchgOperation = new PutCompareExchangeValueOperation(cmpXchgKey, cmpXchgValue, 0);
 
     const putCmpXchgResult = await documentStore.operations.send(putCmpXchgOperation);
+    //endregion
 
+    //region Step_2
     const success = putCmpXchgResult.successful;
     const putValue = putCmpXchgResult.value;
     const putVersion = putCmpXchgResult.index;
@@ -24,16 +27,21 @@ async function run ({ cmpXchgKey, cmpXchgValue }) {
     if (!success) {
         result = 'Key already exists';
     }
+    //endregion
 
+    //region Step_3
     const getCmpXchgOperation = new GetCompareExchangeValueOperation(cmpXchgKey, String);
 
     const getCmpXchgResult = await documentStore.operations.send(getCmpXchgOperation);
+    //endregion
 
+    //region Step_4
     const key = getCmpXchgResult.key;
     const currentValue = getCmpXchgResult.value;
     const currentValueVersion = getCmpXchgResult.index;
-
     const currentMetadata = getCmpXchgResult.metadata;
+    //endregion
+    //endregion
 
     result = result || 'Created a new Compare-Exchange Key: ' + key + ', Value: ' + currentValue + ', Value Version: ' + currentValueVersion;
 
