b31c833fa4daaa7c04228c4cf11956caae44066d
danielle9897 <danielle.9897@gmail.com>
2022-08-18 14:36:35 +0300

    HRINT-2968  Highlighting Query Results - Map-Reduce Index

--- DemoServer/AdditionalLanguages/nodejs/demo/textSearch/highlightQueryResultsMapReduce/index.js
+++ DemoServer/AdditionalLanguages/nodejs/demo/textSearch/highlightQueryResultsMapReduce/index.js
@@ -1,41 +1,52 @@
-const {
-    documentStore,
-    mediaStore
-} = require('../../common/docStoreHolder');
+const { mediaStore } = require('../../common/docStoreHolder');
+//region Usings
 const { AbstractJavaScriptIndexCreationTask } = require('ravendb');
+//endregion
 
+//region Demo
+//region Step_1
 class ArtistsAllSongs extends AbstractJavaScriptIndexCreationTask {
+//endregion
     constructor () {
         super();
 
+        //region Step_2
         this.map('lastFms', song => {
             return {
                 artist: song.Artist,
                 allSongTitles: song.Title
             };
         });
+        //endregion
 
+        //region Step_3
         this.reduce(results => results.groupBy(result => result.artist).aggregate(g => {
             return {
                 artist: g.key,
                 allSongTitles: g.values.map(x => x.allSongTitles).join(' ')
             };
         }));
+        //endregion
 
+        //region Step_4
         this.store('artist', 'Yes');
 
         this.store('allSongTitles', 'Yes');
         this.index('allSongTitles', 'Search');
         this.termVector('allSongTitles', 'WithPositionsAndOffsets');
+        //endregion
     }
 }
 
+//region Step_5
 class IndexEntry {
     constructor () {
         this.artist = null;
         this.allSongTitles = null;
     }
 }
+//endregion
+//endregion
 
 async function run ({ searchTerm, preTag, postTag, fragmentLength, fragmentCount }) {
     searchTerm = searchTerm || 'smile';
@@ -44,15 +55,19 @@ async function run ({ searchTerm, preTag, postTag, fragmentLength, fragmentCount
     fragmentLength = fragmentLength || 80;
     fragmentCount = fragmentCount || 1;
 
+    //region Demo
     const session = mediaStore.openSession();
 
-    let highlightingsInfo;
-
+    //region Step_6
     const highlightingOptions = {
         groupKey: 'artist',
         preTags: [preTag],
         postTags: [postTag]
     };
+    //endregion
+
+    //region Step_7
+    let highlightingsInfo;
 
     const artistsResults = await session.query(IndexEntry, ArtistsAllSongs)
         .highlight({
@@ -65,10 +80,14 @@ async function run ({ searchTerm, preTag, postTag, fragmentLength, fragmentCount
         })
         .search('allSongTitles', searchTerm)
         .all();
+    //endregion
 
+    //region Step_8
     if (artistsResults.length > 0) {
         const songsFragments = highlightingsInfo.getFragments(artistsResults[0].artist);
     }
+    //endregion
+    //endregion
 
     const highlightResults = [];
 
