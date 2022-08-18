4256f867e3bb2351242d7201852018025b184887
Marcin Lewandowski <marcin@ravendb.net>
2022-08-18 14:15:16 +0200

    bump npm packages

--- DemoServer/AdditionalLanguages/nodejs/demo/autoIndexes/autoMapReduceIndex/index.js
+++ DemoServer/AdditionalLanguages/nodejs/demo/autoIndexes/autoMapReduceIndex/index.js
@@ -7,7 +7,7 @@ async function run () {
     //region Step_1
     const numberOfEmployeesPerCountry = await session.query({ collection: 'employees' })
     //endregion
-        //region Step_2    
+        //region Step_2
         .groupBy('Address.Country')
         //endregion
         //region Step_3
@@ -20,21 +20,21 @@ async function run () {
         //region Step_5
         .all();
         //endregion
-    
-        //region Step_6
-        //   [
-        //     {
-        //       Country: 'USA',
-        //       NumberOfEmployees: 5,
-        //       '@metadata': { '@projection': true, '@change-vector': null, '@index-score': 1 }
-        //     },
-        //     {
-        //       Country: 'UK',
-        //       NumberOfEmployees: 4,
-        //       '@metadata': { '@projection': true, '@change-vector': null, '@index-score': 1 }
-        //     }
-        //   ]
-        //endregion    
+
+    //region Step_6
+    //   [
+    //     {
+    //       Country: 'USA',
+    //       NumberOfEmployees: 5,
+    //       '@metadata': { '@projection': true, '@change-vector': null, '@index-score': 1 }
+    //     },
+    //     {
+    //       Country: 'UK',
+    //       NumberOfEmployees: 4,
+    //       '@metadata': { '@projection': true, '@change-vector': null, '@index-score': 1 }
+    //     }
+    //   ]
+    //endregion
     //endregion
 
     return numberOfEmployeesPerCountry;
