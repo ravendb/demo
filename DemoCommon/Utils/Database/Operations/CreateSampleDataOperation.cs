3077d6522f7f661b7ed6f3ac35c015c7d467d017
danielle9897 <danielle.9897@gmail.com>
2022-08-28 13:00:57 +0300

    HRINT-2995 [Demo] Remove operation type Indexes when creating the sample data for a demo

--- DemoCommon/Utils/Database/Operations/CreateSampleDataOperation.cs
+++ DemoCommon/Utils/Database/Operations/CreateSampleDataOperation.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Net.Http;
+using System.Text;
 using Raven.Client.Documents.Conventions;
 using Raven.Client.Documents.Operations;
+using Raven.Client.Documents.Smuggler;
 using Raven.Client.Http;
 using Sparrow.Json;
 
@@ -9,18 +11,46 @@ namespace DemoCommon.Utils.Database.Operations
 {
     public class CreateSampleDataOperation : IMaintenanceOperation
     {
+        private readonly DatabaseItemType _operateOnTypes;
+        
+        public CreateSampleDataOperation(DatabaseItemType operateOnTypes = DatabaseItemType.Documents |
+                                                                           DatabaseItemType.Attachments |
+                                                                           DatabaseItemType.CounterGroups |
+                                                                           DatabaseItemType.TimeSeries |
+                                                                           DatabaseItemType.CompareExchange)
+        {
+            _operateOnTypes = operateOnTypes;
+        }
+        
         public RavenCommand GetCommand(DocumentConventions conventions, JsonOperationContext context)
         {
-            return new CreateSampleDataCommand();
+            return new CreateSampleDataCommand(_operateOnTypes);
         }
 
         private class CreateSampleDataCommand : RavenCommand, IRaftCommand
         {
+            private readonly DatabaseItemType _operateOnTypes;
+
+            public CreateSampleDataCommand(DatabaseItemType operateOnTypes)
+            {
+                _operateOnTypes = operateOnTypes;
+            }
+            
             public override bool IsReadRequest => false;
 
             public override HttpRequestMessage CreateRequest(JsonOperationContext ctx, ServerNode node, out string url)
             {
-                url = $"{node.Url}/databases/{node.Database}/studio/sample-data";
+                var sb = new StringBuilder($"{node.Url}/databases/{node.Database}/studio/sample-data");
+
+                var operateOnTypes = _operateOnTypes.ToString().Split(",", StringSplitOptions.RemoveEmptyEntries);
+                for (var i = 0; i < operateOnTypes.Length; i++)
+                {
+                    sb.Append(i == 0 ? "?" : "&");
+                    sb.Append("operateOnTypes=");
+                    sb.Append(operateOnTypes[i].Trim());
+                }
+
+                url = sb.ToString();
 
                 return new HttpRequestMessage
                 {
