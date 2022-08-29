db60c80ae184db8700caf40989da8adab5fb86b7
danielle9897 <danielle.9897@gmail.com>
2022-08-29 15:49:02 +0300

    HRINT-2985 Fixes for FacetsFromDocument Demo

--- DemoServer/AdditionalLanguages/java/src/test/java/net/ravendb/demo/test/facetedSearch/FacetsFromDocumentTest.java
+++ DemoServer/AdditionalLanguages/java/src/test/java/net/ravendb/demo/test/facetedSearch/FacetsFromDocumentTest.java
@@ -1,22 +1,21 @@
 package net.ravendb.demo.test.facetedSearch;
 
-import net.ravendb.demo.facetedSearch.facetsBasics.FacetsBasics;
+import net.ravendb.demo.common.DocumentStoreHolder;
 import net.ravendb.demo.facetedSearch.facetsFromDocument.FacetsFromDocument;
 import org.junit.Assert;
 import org.junit.Test;
-
 import java.util.List;
 
 public class FacetsFromDocumentTest {
 
     @Test
     public void test() {
+        DocumentStoreHolder.store.executeIndex(new FacetsFromDocument.Products_ByCategoryAndPrice());
+
         FacetsFromDocument.RunParams runParams = new FacetsFromDocument.RunParams();
-        List<FacetsFromDocument.MyFacetResult> results =
-            new FacetsFromDocument().run(runParams);
+        List<FacetsFromDocument.MyFacetResult> results = new FacetsFromDocument().run(runParams);
 
         Assert.assertNotNull(results);
         Assert.assertEquals(results.size(), 12);
     }
-
 }
