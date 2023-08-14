ca9ba122a5e4ab0a95c018dfd6446221e07399bc
danielle9897 <danielle.9897@gmail.com>
2023-08-14 17:18:01 +0300

    RDoc-2482 Python demos - Static Indexes Overview

---
+++ DemoServer/AdditionalLanguages/python/staticIndexes/staticIndexesOverview/staticIndexesOverview.py
@@ -0,0 +1,36 @@
+from demo_example import Example
+from models import Employee
+
+#region Usings
+from ravendb import AbstractIndexCreationTask
+#endregion
+
+#region Demo
+#region Step_1
+class Employees_ByLastName(AbstractIndexCreationTask):
+#endregion
+    #region Step_2
+    def __init__(self):
+        super().__init__()
+        # Define:
+        #    Map(s) functions
+        #    Reduce function
+        #    Additional indexing options per field
+    #endregion
+#endregion
+
+class StaticIndexesOverview(Example):
+    def run(self, run_params=None):
+        #region Demo
+        #region Step_3
+        Employees_ByLastName().execute(self.document_store_holder.store())
+        #endregion
+
+        with (self.document_store_holder.store().open_session() as session):
+            #region Step_4
+            query_on_index = session.query_index_type(Employees_ByLastName, Employee)\
+                .where_equals("LastName", "SomeName")
+
+            query_results = list(query_on_index)
+            #endregion
+        #endregion
