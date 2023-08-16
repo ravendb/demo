cd5b42235813788f7c9d43f8ad59f4af87de44de
danielle9897 <danielle.9897@gmail.com>
2023-08-16 17:08:26 +0300

    RDoc-2482 Python demos - FTS with Static Index - Multiple Fields

--- DemoServer/AdditionalLanguages/python/textSearch/fTSWithStaticIndexMultipleFields/ftsWithStaticIndexMultipleFields.py
+++ DemoServer/AdditionalLanguages/python/textSearch/fTSWithStaticIndexMultipleFields/ftsWithStaticIndexMultipleFields.py
@@ -1,5 +1,5 @@
 from demo_example import Example, RunParamsBase
-from models import Category
+from models import LastFm
 
 #region Usings
 from ravendb import AbstractIndexCreationTask
@@ -9,40 +9,45 @@ from ravendb.documents.indexes.definitions import FieldIndexing
 class RunParams(RunParamsBase):
     def __init__(self, search_term: str = None):
         self.search_term = search_term
-
+        
 #region Demo
 #region Step_1
-class Categories_DescriptionText(AbstractIndexCreationTask):
+class Song_TextData(AbstractIndexCreationTask):
 #endregion
     def __init__(self):
         #region Step_2
         super().__init__()
         self.map = (
-            "docs.Products.Select(product => new { "
-            "    CategoryDescription = category.Description "
+            "docs.LastFms.Select(song => new { "
+            "    SongData = new object[] { "
+            "        song.Artist, "
+            "        song.Title, "
+            "        song.Tags, "
+            "        song.TrackId "
+            "    } "
             "})"
-        )        
+        )
         #endregion
 
         #region Step_3
-        self._index("CategoryDescription", FieldIndexing.SEARCH)
+        self._index("SongData", FieldIndexing.SEARCH)
         #endregion
 #endregion
 
-class FtsWithStaticIndexSingleField(Example):
+class FtsWithStaticIndexMultipleFields(Example):
     def run(self, run_params: RunParams):
         searchTerm = run_params.search_term
-        Categories_DescriptionText().execute(self.document_store_holder.store())
+        Song_TextData().execute(self.document_store_holder.store())
 
         #region Demo
         with self.document_store_holder.store().open_session() as session:
             #region Step_4
-            categories_with_search_term = list(
-                session.query_index_type(Categories_DescriptionText, Category).search(
-                    "CategoryDescription", searchTerm
-                )
+            results = list(
+                session.query_index_type(Song_TextData, LastFm)
+                    .search("SongData", searchTerm)
+                    .take(20)
             )
             #endregion
         #endregion
 
-        return categories_with_search_term
+        return results
