4a018661cc24f54048f9ed3fce2b0aa6fe4fc1de
danielle9897 <danielle.9897@gmail.com>
2023-10-05 14:40:43 +0300

    RDoc-2526 PHP demos - Load & Edit Document

---
+++ DemoServer/AdditionalLanguages/php/basics/loadAndEditDocument/LoadAndEditDocument.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace RavenDB\Demo\basics\loadAndEditDocument;
+
+//region Usings
+use RavenDB\Documents\Session\DocumentSessionInterface;
+//endregion
+Use RavenDB\Demo\common\DocumentStoreHolder;
+use RavenDB\Demo\common\models\Company;
+
+class LoadAndEditDocument
+{
+    public function __invoke(RunParams $runParams)
+    {
+        $companyName = $runParams->getCompanyName();
+
+        //region Demo
+        $session = DocumentStoreHolder::getStore()->openSession();
+        try {
+            //region Step_1
+            $company = $session->load(Company::class, "companies/5-A");
+            //endregion
+
+            //region Step_2
+            $company->setName($companyName);
+            //endregion
+
+            //region Step_3
+            $session->saveChanges();
+            //endregion
+        } finally {
+            $session->close();
+        }
+        //endregion
+    }
+}
